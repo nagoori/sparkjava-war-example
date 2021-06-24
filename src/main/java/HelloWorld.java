fdbdda428f4cbd1f3ca37ca9319d37ead45e1cd5
nagoori <reddy.nagoori@gmail.com>
2021-06-25 00:18:54 +0530

    added ip file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World Kimsu");
+		get("/hello", (req, res) -> "Hello World added two ips");
 	}
 }
